1560fc2af4dd609477cd891cc13653e6713588e9
Dablakbandit <ashle.thew@gmail.com>
2018-07-21 11:23:53 +1200

    1.13 changes

--- src/main/java/me/dablakbandit/core/utils/Version.java
+++ src/main/java/me/dablakbandit/core/utils/Version.java
@@ -10,7 +10,6 @@ public class Version{
 	
 	private static boolean nine = isNine();
 	
-	@SuppressWarnings("unused")
 	private static boolean isNine(){
 		try{
 			Material m = Material.ELYTRA;
@@ -22,5 +21,20 @@ public class Version{
 	
 	public static boolean isAtleastNine(){
 		return nine;
+	}
+	
+	private static boolean thirteen = isThirteen();
+	
+	private static boolean isThirteen(){
+		try{
+			Material m = Material.BLACK_STAINED_GLASS_PANE;
+			return true;
+		}catch(Throwable e){
+		}
+		return false;
+	}
+	
+	public static boolean isAtleastThirteen(){
+		return thirteen;
 	}
 }
