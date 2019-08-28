cb7473169370d0e4a34118fe36691554181bca2e
Dablakbandit <ashle.thew@gmail.com>
2019-08-29 06:12:46 +1200

    Fixes for 1.14 linux epol
    Fixes for 1.14 linux epol

--- src/main/java/me/dablakbandit/core/server/packet/ServerWrapper.java
+++ src/main/java/me/dablakbandit/core/server/packet/ServerWrapper.java
@@ -19,37 +19,33 @@ import me.dablakbandit.core.utils.NMSUtils;
 
 public class ServerWrapper{
 	
-	private static Class<?>	classServerConnection		= NMSUtils.getNMSClass("ServerConnection");
-	private static Class<?>	classMinecraftServer		= NMSUtils.getNMSClass("MinecraftServer");
-	private static Class<?>	classNetworkManager			= NMSUtils.getNMSClass("NetworkManager");
-	private static Class<?>	classLegacyPingHandler		= NMSUtils.getNMSClass("LegacyPingHandler");
-	private static Class<?>	classPacketSplitter			= NMSUtils.getNMSClass("PacketSplitter");
-	private static Class<?>	classPacketDecoder			= NMSUtils.getNMSClass("PacketDecoder");
-	private static Class<?>	classPacketPrepender		= NMSUtils.getNMSClass("PacketPrepender");
-	private static Class<?>	classPacketEncoder			= NMSUtils.getNMSClass("PacketEncoder");
-	private static Class<?>	classEnumProtocolDirection	= NMSUtils.getNMSClass("EnumProtocolDirection");
-	private static Class<?>	classPropertyManager		= NMSUtils.getNMSClassSilent("DedicatedServerSettings");
+	private static Class<?>		classServerConnection			= NMSUtils.getNMSClass("ServerConnection");
+	private static Class<?>		classMinecraftServer			= NMSUtils.getNMSClass("MinecraftServer");
+	private static Class<?>		classNetworkManager				= NMSUtils.getNMSClass("NetworkManager");
+	private static Class<?>		classLegacyPingHandler			= NMSUtils.getNMSClass("LegacyPingHandler");
+	private static Class<?>		classPacketSplitter				= NMSUtils.getNMSClass("PacketSplitter");
+	private static Class<?>		classPacketDecoder				= NMSUtils.getNMSClass("PacketDecoder");
+	private static Class<?>		classPacketPrepender			= NMSUtils.getNMSClass("PacketPrepender");
+	private static Class<?>		classPacketEncoder				= NMSUtils.getNMSClass("PacketEncoder");
+	private static Class<?>		classEnumProtocolDirection		= NMSUtils.getNMSClass("EnumProtocolDirection");
+	private static Class<?>		classDedicatedServerSettings	= NMSUtils.getNMSClassSilent("DedicatedServerSettings");
+	private static Class<?>		classPropertyManager			= NMSUtils.getNMSClassSilent("PropertyManager");
 	
-	static{
-		if(classPropertyManager == null){
-			classPropertyManager = NMSUtils.getNMSClass("PropertyManager");
-		}
-	}
-	private static Class<?>		classPacketListener		= NMSUtils.getNMSClass("PacketListener");
-	private static Class<?>		classLazyInitVar		= NMSUtils.getNMSClass("LazyInitVar");
-	private static Class<?>		classHandshakeListener	= NMSUtils.getNMSClass("HandshakeListener");
+	private static Class<?>		classPacketListener				= NMSUtils.getNMSClass("PacketListener");
+	private static Class<?>		classLazyInitVar				= NMSUtils.getNMSClass("LazyInitVar");
+	private static Class<?>		classHandshakeListener			= NMSUtils.getNMSClass("HandshakeListener");
 	
-	private static Constructor	conNetworkManager		= NMSUtils.getConstructor(classNetworkManager, classEnumProtocolDirection);
-	private static Constructor	conLegacyPingHandler	= NMSUtils.getConstructor(classLegacyPingHandler, classServerConnection);
-	private static Constructor	conPacketDecoder		= NMSUtils.getConstructor(classPacketDecoder, classEnumProtocolDirection);
-	private static Constructor	conPacketEncoder		= NMSUtils.getConstructor(classPacketEncoder, classEnumProtocolDirection);
-	private static Constructor	conPacketSplitter		= NMSUtils.getConstructor(classPacketSplitter);
-	private static Constructor	conPacketPrepender		= NMSUtils.getConstructor(classPacketPrepender);
+	private static Constructor	conNetworkManager				= NMSUtils.getConstructor(classNetworkManager, classEnumProtocolDirection);
+	private static Constructor	conLegacyPingHandler			= NMSUtils.getConstructor(classLegacyPingHandler, classServerConnection);
+	private static Constructor	conPacketDecoder				= NMSUtils.getConstructor(classPacketDecoder, classEnumProtocolDirection);
+	private static Constructor	conPacketEncoder				= NMSUtils.getConstructor(classPacketEncoder, classEnumProtocolDirection);
+	private static Constructor	conPacketSplitter				= NMSUtils.getConstructor(classPacketSplitter);
+	private static Constructor	conPacketPrepender				= NMSUtils.getConstructor(classPacketPrepender);
 	
-	private static Constructor	conHandshakeListener	= NMSUtils.getConstructor(classHandshakeListener, classMinecraftServer, classNetworkManager);
+	private static Constructor	conHandshakeListener			= NMSUtils.getConstructor(classHandshakeListener, classMinecraftServer, classNetworkManager);
 	
-	private static Method		getBoolean				= NMSUtils.getMethod(classPropertyManager, "getBoolean", String.class, boolean.class);
-	private static Method		methodC					= NMSUtils.getMethod(classLazyInitVar, "c");
+	private static Method		getBoolean						= NMSUtils.getMethod(classPropertyManager, "getBoolean", String.class, boolean.class);
+	private static Method		methodC							= NMSUtils.getMethod(classLazyInitVar, "c");
 	
 	static{
 		if(methodC == null){
@@ -83,7 +79,10 @@ public class ServerWrapper{
 	public ServerWrapper(Object dedicatedserver){
 		try{
 			this.dedicatedserver = dedicatedserver;
-			propertymanager = NMSUtils.getFirstFieldOfType(dedicatedserver.getClass(), classPropertyManager).get(dedicatedserver);
+			propertymanager = NMSUtils.getFirstFieldOfType(dedicatedserver.getClass(), classDedicatedServerSettings != null ? classDedicatedServerSettings : classPropertyManager).get(dedicatedserver);
+			if(classDedicatedServerSettings != null){
+				propertymanager = NMSUtils.getFirstFieldOfType(classDedicatedServerSettings, NMSUtils.getNMSClass("DedicatedServerProperties")).get(propertymanager);
+			}
 			serverconnection = NMSUtils.getFirstFieldOfType(classMinecraftServer, classServerConnection).get(dedicatedserver);
 		}catch(Exception e){
 			e.printStackTrace();
@@ -203,12 +202,14 @@ public class ServerWrapper{
 	
 	public Class<? extends ServerChannel> getSocketClass(){
 		try{
-			if((Epoll.isAvailable()) && (boolean)getBoolean.invoke(propertymanager, "use-native-transport", true)){
+			if((boolean)getBoolean.invoke(propertymanager, "use-native-transport", true) && (Epoll.isAvailable())){
 				return EpollServerSocketChannel.class;
 			}else{
 				return NioServerSocketChannel.class;
 			}
 		}catch(Exception e){
+			System.out.println(propertymanager);
+			System.out.println(getBoolean);
 			e.printStackTrace();
 		}
 		return null;
@@ -216,7 +217,7 @@ public class ServerWrapper{
 	
 	public Object getLazyInitVar(){
 		try{
-			if((Epoll.isAvailable()) && (boolean)getBoolean.invoke(propertymanager, "use-native-transport", true)){
+			if((boolean)getBoolean.invoke(propertymanager, "use-native-transport", true) && (Epoll.isAvailable())){
 				return fieldB.get(null);
 			}else{
 				return fieldA.get(null);
