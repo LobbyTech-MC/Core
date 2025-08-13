5b8f229ae4926fb1d978dcbd6c28a407a61a401b
LobbyTech <1187755698@qq.com>
2025-08-14 04:10:00 +0800

    1

--- src/main/java/me/dablakbandit/core/config/path/IntegerPath.java
+++ src/main/java/me/dablakbandit/core/config/path/IntegerPath.java
@@ -13,9 +13,7 @@ public class IntegerPath extends Path<Integer>{
 	}
 	
 	protected Integer get(RawConfiguration config, String path){
-		if (config == null) {
-			return 0;
-		}
+
 		return config.getInt(path);
 	}
 }
